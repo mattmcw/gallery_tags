26e0a60447eb7560bf5bf785801f5309f9e64757
mattmcw <matt@sixteenmillimeter.com>
2024-11-23 20:04:24 -0500

    Layout and print the tags.

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,5 @@
 var dropZone = document.getElementById('drop-zone');
+var tags = document.getElementById('tags');
 dropZone.addEventListener('dragover', function (event) {
     event.preventDefault();
     dropZone.style.backgroundColor = '#e0f7fa';
@@ -12,25 +13,67 @@ dropZone.addEventListener('dragenter', function (event) {
 dropZone.addEventListener('drop', function (event) {
     event.preventDefault();
     dropZone.style.backgroundColor = '#f9f9f9';
-    console.log('drop');
-    console.dir(event.dataTransfer.files.length);
     if (event.dataTransfer.files.length > 0) {
-        if (event.dataTransfer.files[0].type === 'text/csv') {
+        if (event.dataTransfer.files[0].type === 'text/tab-separated-values') {
             var reader = new FileReader();
             reader.onload = function (e) {
                 var content = e.target.result;
-                var parsedData = parseCSV(content);
-                console.dir(parsedData);
+                var parsedData = parseTSV(content);
+                displayTags(parsedData);
             };
             reader.readAsText(event.dataTransfer.files[0]);
         }
         else {
-            alert('Please drop a CSV file.');
+            console.dir(event.dataTransfer.files[0].type);
+            alert('Please drop a TSV file.');
         }
     }
 });
-function parseCSV(csvText) {
-    var rows = csvText.split('\n');
-    var parsedData = rows.map(function (row) { return row.split(','); });
-    return parsedData;
+function parseTSV(tsvText) {
+    var rows = tsvText.split('\n');
+    var parsedData = rows.map(function (row) { return row.split('\t'); });
+    var objs = [];
+    var obj;
+    for (var i = 1; i < parsedData.length; i++) {
+        obj = {};
+        for (var x = 0; x < parsedData[i].length; x++) {
+            obj[(parsedData[0][x] + '').trim().replace(/\"/g, "")] = parsedData[i][x].trim();
+        }
+        objs.push(obj);
+    }
+    return objs;
+}
+function displayTag(row) {
+    var tag = document.createElement('div');
+    var artist = document.createElement('div');
+    var title = document.createElement('div');
+    var date = document.createElement('div');
+    var medium = document.createElement('div');
+    var price = document.createElement('div');
+    tag.classList.add('tag');
+    artist.classList.add('artist');
+    artist.innerHTML = row.Artist;
+    title.classList.add('title');
+    title.innerHTML = row.Title;
+    date.classList.add('date');
+    date.innerHTML = row.Date;
+    medium.classList.add('medium');
+    medium.innerHTML = row.Medium;
+    price.classList.add('price');
+    price.innerHTML = row.Price;
+    tag.appendChild(artist);
+    tag.appendChild(title);
+    tag.appendChild(date);
+    tag.appendChild(medium);
+    tag.appendChild(price);
+    tags.appendChild(tag);
+}
+function displayTags(rows) {
+    document.getElementById('dnd').classList.add('hide');
+    document.getElementById('display').classList.remove('hide');
+    for (var _i = 0, rows_1 = rows; _i < rows_1.length; _i++) {
+        var row = rows_1[_i];
+        console.dir(row);
+        displayTag(row);
+    }
 }
--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,5 @@
 const dropZone = document.getElementById('drop-zone');
+const tags = document.getElementById('tags');
 
 dropZone.addEventListener('dragover', (event) => {
     event.preventDefault();
@@ -15,25 +16,77 @@ dropZone.addEventListener('dragenter', (event) => {
 dropZone.addEventListener('drop', (event) => {
     event.preventDefault();
     dropZone.style.backgroundColor = '#f9f9f9';
-    console.log('drop');
-    console.dir(event.dataTransfer.files.length)
     if (event.dataTransfer.files.length > 0) {
-        if (event.dataTransfer.files[0].type === 'text/csv') {
+        if (event.dataTransfer.files[0].type === 'text/tab-separated-values') {
             const reader = new FileReader();
             reader.onload = function(e) {
                 const content = e.target.result;
-                const parsedData = parseCSV(content);
-                console.dir(parsedData);
+                const parsedData = parseTSV(content as string);
+                displayTags(parsedData);
             };
             reader.readAsText(event.dataTransfer.files[0]);
         } else {
-            alert('Please drop a CSV file.');
+            console.dir(event.dataTransfer.files[0].type);
+            alert('Please drop a TSV file.');
         }
     }
 });
 
-function parseCSV(csvText) {
-    const rows = csvText.split('\n');
-    const parsedData = rows.map(row => row.split(','));
-    return parsedData;
+function parseTSV (tsvText : string) {
+    const rows = tsvText.split('\n');
+    const parsedData = rows.map(row => row.split('\t'));
+    const objs : any[] = [];
+    let obj : any;
+    for (let i = 1; i < parsedData.length; i++) {
+        obj = {};
+        for (let x = 0; x < parsedData[i].length; x++) {
+            obj[(parsedData[0][x] + '').trim().replace(/\"/g, "")] = parsedData[i][x].trim();
+        }
+        objs.push(obj);
+    }
+    return objs;
+}
+
+function displayTag (row : any) {
+    const tag : HTMLElement = document.createElement('div');
+    const artist : HTMLElement = document.createElement('div');
+    const title : HTMLElement = document.createElement('div');
+    const date : HTMLElement = document.createElement('div');
+    const medium : HTMLElement = document.createElement('div');
+    const price : HTMLElement = document.createElement('div');
+    
+    tag.classList.add('tag');
+
+    artist.classList.add('artist');
+    artist.innerHTML = row.Artist;
+
+    title.classList.add('title');
+    title.innerHTML = row.Title;
+
+    date.classList.add('date');
+    date.innerHTML = row.Date;
+
+    medium.classList.add('medium');
+    medium.innerHTML = row.Medium;
+
+    price.classList.add('price');
+    price.innerHTML = row.Price;
+
+    tag.appendChild(artist);
+    tag.appendChild(title);
+    tag.appendChild(date); 
+    tag.appendChild(medium);
+    tag.appendChild(price);
+
+    tags.appendChild(tag);
+}
+
+function displayTags (rows : any[]) {
+    document.getElementById('dnd').classList.add('hide');
+    document.getElementById('display').classList.remove('hide');
+
+    for (let row of rows) {
+        console.dir(row)
+        displayTag(row);
+    }
 }
